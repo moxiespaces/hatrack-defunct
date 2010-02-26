708a7d4733785e67763943bf8fd5ff5461ed8b2b
terry <theath@gmail.com>
2010-02-26 11:08:06 -0600

    mongo support for mongohq

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,6 @@
 config = YAML.load(File.read(File.join(Rails.root, "config", "mongo.yml")))
 MongoMapper.setup(config, Rails.env)
+MongoMapper.database.authenticate('theath', 'omgmongo') if Rails.env == "production"
 
 module IdentityMapAddition
   def self.included(model)
