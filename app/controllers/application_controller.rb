3c7e1bc2eccf289e208e3140a440f131f309c089
terry <theath@gmail.com>
2010-02-26 22:49:36 -0600

    reset id map per req

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
-
+  before_filter :clear_identity_map
+  
+  def clear_identity_map
+    MongoMapper::Plugins::IdentityMap.models.each{|m| m.identity_map.clear}
+  end
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
