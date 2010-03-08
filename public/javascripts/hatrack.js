978b7e5ea5542ab9e0631a32610527dcfbd9e49b
Rodrigo DeJuana <rdejuana@rdejuana-ngenera.local>
2010-03-08 17:33:38 -0600

    skinned it some

--- public/javascripts/hatrack.js
+++ public/javascripts/hatrack.js
@@ -7,7 +7,7 @@ var Hatrack = {
   init: function(){
     $('#current_name').bind('click',Hatrack.showSprints);
 
-    $('.add_hat').bind('click', Hatrack.addHat);
+    $('.add_hat').bind('click', Hatrack.determineColor);
 
     $('.toggle').bind('click', Hatrack.toggleColor);
 
@@ -27,19 +27,31 @@ var Hatrack = {
   setup: function(editable,hat) {
     for (var i in Hatrack.colors) {
       $('.hat.e.' + Hatrack.colors[i]).editable('/hats/update?sprint_id=' + Hatrack.sprint_id + '&type=' + Hatrack.colors[i], {
-        type: 'textarea',submit: 'Save',cancel: 'Cancel', onblur: 'ignore',onedit:Hatrack.onEdit , onreset: Hatrack.setup});
+        type: 'textarea',submit: 'Save',cancel: 'Cancel',
+        onblur: 'ignore',onedit:Hatrack.onEdit , onreset: Hatrack.setup,
+        placeholder: 'Enter a hat'
+      });
     }
 
-    $('.green_hat div').editable('/hats/update_green_hat?sprint_id=' + Hatrack.sprint_id, {
-      type: 'textarea',submit: 'Save',cancel: 'Cancel', onblur: 'ignore',onedit:Hatrack.onEdit, onreset: Hatrack.setup });
+    var green_opts =  {
+      type: 'textarea',submit: 'Save',cancel: 'Cancel', onblur: 'ignore',onedit:Hatrack.onEdit,
+      placeholder: 'Add a green hat',onreset: Hatrack.setup }
+
+    $('.green_hat .text').editable('/hats/update_green_hat?sprint_id=' + Hatrack.sprint_id, green_opts);
+    green_opts.placeholder = 'Add a green hat owner'
+    $('.green_hat .owner').editable('/hats/update_green_hat?sprint_id=' + Hatrack.sprint_id, green_opts);
 
 
     Hatrack.screen_dirty = $('.hat textarea').length > 1;
 
   },
 
-  addHat: function(event) {
-    var color = event.target.parentNode.className.split(' ')[1];
+  determineColor:function(event){
+    Hatrack.addHat($(event.target).val());
+  },
+  addHat: function(color) {
+    Hatrack.resetNewHat();
+    
     if ($('.hat.new.' + color).length > 0) {
       $('.hat.new.' + color).click();
       return false;
@@ -47,10 +59,12 @@ var Hatrack = {
 
     var id = Math.floor(Math.random() * 1111)
 
-    $('#' + color + ' ul').prepend('<li class="hat_item"><div class="hat new ' + color + '" id="hat_' + id + '">Enter a hat</div></li>');
+    $('#' + color + ' ul').prepend('<li class="hat_item"><div class="hat new ' + color + '" id="hat_' + id + '"></div></li>');
     var new_hat = $('#hat_' + id);
     new_hat.editable('/hats/create?sprint_id=' + Hatrack.sprint_id + '&type=' + color, {
-      type: 'textarea',submit: 'Save',cancel: 'Cancel',onedit:Hatrack.onNewEdit, onreset: Hatrack.setup, onblur: 'ignore', callback:function(value, settings) {
+      type: 'textarea',submit: 'Save',cancel: 'Cancel',onedit:Hatrack.onNewEdit,
+      onreset: Hatrack.setup, onblur: 'ignore',placeholder: 'Enter a hat', 
+      callback:function(value, settings) {
         new_hat.parent('li').replaceWith(value);
         Hatrack.setup();
       }});
@@ -68,7 +82,16 @@ var Hatrack = {
   },
 
   toggleColor: function(event) {
-    $('#' + event.target.className.split(' ')[1]).toggle();
+    var color = event.target.className.split(' ')[1];
+    $('#' + color).toggle();
+
+    $('.rack:visible').each(function(i,e){
+      $('.toggle.'+e.id).removeClass('disabled');
+    });
+    $('.rack:hidden').each(function(i,e){
+      $('.toggle.'+e.id).addClass('disabled');
+    });
+
     return false;
   },
 
@@ -113,33 +136,30 @@ var Hatrack = {
 
   screen_dirty: false,
   new_hat_enabled: false,
-  mock_add_event: {target:{parentNode:{className:""}}},
   keyWatcher: function(event) {
     if ((!Hatrack.screen_dirty && event.keyCode == 72) ||
         (event.altKey && event.keyCode == 72 )) {
-      $('#new_hat').addClass('enabled');
+      $('#new_hat').show();
       Hatrack.new_hat_enabled = true;
       return false;
     }
 
     if (Hatrack.new_hat_enabled) {
-      var mock_event = null;
+      var color = null;
       switch (event.keyCode) {
         case 66: event.target.parentNode.className
-          mock_event = Hatrack.mock_add_event;
-          mock_event.target.parentNode.className = "add_hat black";
+          color = "black";
           break;
         case 89:
-          mock_event = Hatrack.mock_add_event;
-          mock_event.target.parentNode.className = "add_hat yellow";
+          color = "yellow";
           break;
         case 27:
           Hatrack.resetNewHat();
           return false
           break;
       }
-      if (mock_event) {
-        Hatrack.addHat(mock_event);
+      if (color) {
+        Hatrack.addHat(color);
         Hatrack.resetNewHat();
         return false;
 
@@ -148,7 +168,7 @@ var Hatrack = {
     }
   },
   resetNewHat: function() {
-    $('#new_hat').removeClass('enabled');
+    $('#new_hat').hide();
     Hatrack.new_hat_enabled = false;
   },
 
